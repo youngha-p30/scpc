7a44f7779218066c4689778d96494ae27e12a4b8
youngha-p30 <youngha.p30@outlook.com>
2025-02-09 11:19:59 +0900

    Create 10172.cpp

---
+++ acmicpc.net/step/1/10172.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|";
+}
