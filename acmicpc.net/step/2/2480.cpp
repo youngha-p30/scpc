ce6d99ec4a07b6f6f9d1cd8ff5019171a7175103
박영하 <youngha.p30@outlook.com>
2025-02-09 13:32:00 +0900

    Create 2480.cpp

---
+++ acmicpc.net/step/2/2480.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == b && b == c)
+        cout << 10000 + a * 1000;
+    else if (a == b || a == c)
+        cout << 1000 + a * 100;
+    else if (b == c)
+        cout << 1000 + b * 100;
+    else
+        cout << max({a, b, c}) * 100;
+}
